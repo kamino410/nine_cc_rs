ad30632f60aee567c31db0d398dca49ebcefb412
kamino410 <kamino.dev@gmail.com>
2021-02-19 03:31:57 +0900

    step 4 done

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,43 @@
-enum Token {
+#[derive(Debug)]
+enum TokenType {
+    Num(usize),
     Plus,
     Minus,
-    Num(usize),
+    Space,
+    Unknown,
+}
+
+#[derive(Debug)]
+struct Token {
+    token_type: TokenType,
+    pos: usize,
 }
 
 impl Token {
-    pub fn expect_num(&self, s: &'static str) -> usize {
-        match self {
-            Token::Num(n) => *n,
-            _ => panic!(s),
+    pub fn as_num(&self) -> Option<usize> {
+        match self.token_type {
+            TokenType::Num(n) => Some(n),
+            _ => None,
         }
     }
 }
 
 struct TokenIter<'a> {
     s: &'a str,
+    pos: usize,
 }
 
 impl<'a> Iterator for TokenIter<'a> {
     type Item = Token;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.s = self.s.trim_start();
         if self.s.is_empty() {
             return None;
         }
+
+        let first_non_space_idx = self.s.find(|c| c != ' ').unwrap_or(self.s.len());
+        self.s = self.s.trim_start();
+        self.pos += first_non_space_idx;
 
         let first_non_num_idx = self
             .s
@@ -32,27 +45,46 @@ impl<'a> Iterator for TokenIter<'a> {
             .unwrap_or(self.s.len());
         if first_non_num_idx != 0 {
             let (s_num, s_remains) = self.s.split_at(first_non_num_idx);
+            let front_pos = self.pos;
             self.s = s_remains;
-            return Some(Token::Num(s_num.parse::<usize>().unwrap()));
+            self.pos += first_non_num_idx;
+            return Some(Token {
+                token_type: TokenType::Num(s_num.parse::<usize>().unwrap()),
+                pos: front_pos,
+            });
         }
 
         let (s_op, s_remains) = self.s.split_at(1);
         self.s = s_remains;
-        if s_op == "+" {
-            return Some(Token::Plus);
+        self.pos += 1;
+        match s_op {
+            "+" => Some(Token {
+                token_type: TokenType::Plus,
+                pos: self.pos - 1,
+            }),
+            "-" => Some(Token {
+                token_type: TokenType::Minus,
+                pos: self.pos - 1,
+            }),
+            _ => Some(Token {
+                token_type: TokenType::Unknown,
+                pos: self.pos - 1,
+            }),
         }
-        if s_op == "-" {
-            return Some(Token::Minus);
-        }
-
-        panic!("Unknown token")
     }
 }
 
 impl<'a> TokenIter<'a> {
     fn new(s: &'a str) -> TokenIter<'a> {
-        TokenIter { s: s }
+        TokenIter { s: s, pos: 0 }
     }
+}
+
+fn error<'a>(line: &'a str, message: &'static str, pos: usize) {
+    eprintln!("[Error]");
+    eprintln!("    {}", line);
+    eprintln!("    {: <2$}^ {}", "", message, pos);
+    std::process::exit(1);
 }
 
 fn main() {
@@ -67,21 +99,57 @@ fn main() {
     println!(".globl _main");
     println!("_main:");
 
-    let first_num = token_iter
-        .next()
-        .expect("Source was empty.")
-        .expect_num("The first token was not a number.");
-    println!("  mov rax, {}", first_num);
+    if let Some(first_token) = token_iter.next() {
+        match first_token.token_type {
+            TokenType::Num(num) => println!("  mov rax, {}", num),
+            _ => {}
+        }
+    } else {
+        eprintln!("Empty source.");
+        std::process::exit(1);
+    }
 
-    while let Some(token) = token_iter.next() {
-        let num = token_iter
-            .next()
-            .expect("No token after the operator.")
-            .expect_num("The token after the operator was not a number.");
-        match token {
-            Token::Plus => println!("  add rax, {}", num),
-            Token::Minus => println!("  sub rax, {}", num),
-            _ => panic!("Unexpected operator"),
+    while let Some(op_token) = token_iter.next() {
+        match op_token.token_type {
+            TokenType::Plus => {
+                if let Some(num_token) = token_iter.next() {
+                    if let Some(num) = num_token.as_num() {
+                        println!("  add rax, {}", num);
+                    } else {
+                        error(
+                            arg.as_str(),
+                            "The token after operator must be a number.",
+                            num_token.pos + 1,
+                        );
+                    }
+                } else {
+                    error(
+                        arg.as_str(),
+                        "This token must be an operator.",
+                        op_token.pos,
+                    );
+                }
+            }
+            TokenType::Minus => {
+                if let Some(num_token) = token_iter.next() {
+                    if let Some(num) = num_token.as_num() {
+                        println!("  sub rax, {}", num);
+                    } else {
+                        error(
+                            arg.as_str(),
+                            "The token after operator must be a number.",
+                            num_token.pos + 1,
+                        );
+                    }
+                } else {
+                    error(
+                        arg.as_str(),
+                        "This token must be an operator.",
+                        op_token.pos,
+                    );
+                }
+            }
+            _ => error(arg.as_str(), "Unexpected operator.", op_token.pos),
         }
     }
 
