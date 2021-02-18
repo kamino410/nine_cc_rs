11c093e9f1b0826531582ab80385b38034a57dec
kamino410 <kamino.dev@gmail.com>
2021-02-18 23:46:32 +0900

    step 1 done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    if std::env::args().len() != 2 {
+        panic!("Invalid args");
+    }
+    println!(".intel_syntax noprefix");
+    println!(".globl main");
+    println!("main:");
+    println!(
+        "  mov rax, {}",
+        std::env::args().nth(1).unwrap().parse::<usize>().unwrap()
+    );
+    println!("  ret");
 }
