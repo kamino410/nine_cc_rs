364b3e96d22321421d7ea1548e7fa4624a023d1a
kamino410 <kamino.dev@gmail.com>
2021-02-20 12:27:24 +0900

    step 6 done

--- src/ast.rs
+++ src/ast.rs
@@ -75,6 +75,10 @@ impl Expr {
             Ok(node)
         } else {
             match unused_tokens[0].token_type {
+                TokenType::RBracket => Err(ExprError::new(
+                    "Corresponding ( is missing.",
+                    unused_tokens[0].pos,
+                )),
                 TokenType::Unknown => {
                     Err(ExprError::new("Unknown Operator.", unused_tokens[0].pos))
                 }
@@ -116,14 +120,14 @@ impl Expr {
     }
 
     fn parse_as_term(tokens: &[Token]) -> Result<(Self, &[Token]), ExprError> {
-        let (mut node, mut unused_tokens) = Self::parse_as_factor(tokens)?;
+        let (mut node, mut unused_tokens) = Self::parse_as_unary(tokens)?;
 
         loop {
             if unused_tokens.len() == 0 {
                 break;
             }
             if let Some(op_type) = &unused_tokens[0].as_muldiv_op() {
-                match Self::parse_as_factor(&unused_tokens[1..]) {
+                match Self::parse_as_unary(&unused_tokens[1..]) {
                     Ok((num_expr, tmp_unused_tokens)) => {
                         node =
                             Expr::BinaryOp(Box::new(BinaryOpNode::new(*op_type, node, num_expr)));
@@ -140,21 +144,42 @@ impl Expr {
         Ok((node, unused_tokens))
     }
 
+    fn parse_as_unary(tokens: &[Token]) -> Result<(Self, &[Token]), ExprError> {
+        let token = &tokens
+            .get(0)
+            .ok_or(ExprError::new("An expression is expected.", 0))?;
+        match token.token_type {
+            TokenType::Plus => Self::parse_as_factor(&tokens[1..]),
+            TokenType::Minus => {
+                let (expr, unused_tokens) = Self::parse_as_factor(&tokens[1..])?;
+                let expr = Expr::BinaryOp(Box::new(BinaryOpNode::new(
+                    BinaryOpType::Sub,
+                    Expr::ConstInt(ConstIntNode::new(0i64)),
+                    expr,
+                )));
+                Ok((expr, unused_tokens))
+            }
+            _ => Self::parse_as_factor(&tokens),
+        }
+    }
+
     fn parse_as_factor(tokens: &[Token]) -> Result<(Self, &[Token]), ExprError> {
-        let token = &tokens.get(0).ok_or(ExprError::new("", 0))?;
+        let token = &tokens
+            .get(0)
+            .ok_or(ExprError::new("An expression is expected.", 0))?;
         match token.token_type {
             TokenType::Num(n) => Ok((Expr::ConstInt(ConstIntNode::new(n as i64)), &tokens[1..])),
             TokenType::LBracket => {
                 let (expr, unused_tokens) = Self::parse_as_expr(&tokens[1..])?;
                 let next_token = &unused_tokens
                     .get(0)
-                    .ok_or(ExprError::new("Pair ) is missing.", token.pos))?;
+                    .ok_or(ExprError::new("Corresponding ) is missing.", token.pos))?;
                 match next_token.token_type {
                     TokenType::RBracket => Ok((expr, &unused_tokens[1..])),
                     _ => Err(ExprError::new("Invalid operator.", next_token.pos)),
                 }
             }
-            _ => Err(ExprError::new("The token is not a number.", token.pos)),
+            _ => Err(ExprError::new("A number is expected.", token.pos)),
         }
     }
 }
@@ -174,21 +199,21 @@ fn ast_parse_ok_test2() {
     let _ = Expr::from(&tokens).ok().unwrap();
 }
 #[test]
-fn ast_parse_err_test1() {
+fn ast_parse_ok_test3() {
     use crate::token::{Token, TokenIter};
     let raw_code = String::from("124- -42");
     let tokens = TokenIter::new(raw_code.as_str()).collect::<Vec<Token>>();
     let _ = Expr::from(&tokens).err().unwrap();
 }
 #[test]
-fn ast_parse_err_test2() {
+fn ast_parse_err_test1() {
     use crate::token::{Token, TokenIter};
     let raw_code = String::from("124* (3 + 2");
     let tokens = TokenIter::new(raw_code.as_str()).collect::<Vec<Token>>();
     let _ = Expr::from(&tokens).err().unwrap();
 }
 #[test]
-fn ast_parse_err_test3() {
+fn ast_parse_err_test2() {
     use crate::token::{Token, TokenIter};
     let raw_code = String::from("124* (3 + 2))");
     let tokens = TokenIter::new(raw_code.as_str()).collect::<Vec<Token>>();
